642cb9bb8a10c25f31aff623a4c84b972896585b
Carlos Gabaldon <cgabaldon@gmail.com>
2012-04-24 21:40:19 -0700

    Issue #1 and #2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120423001124) do
+ActiveRecord::Schema.define(:version => 20120425043350) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "projects_users", :id => false, :force => true do |t|
+    t.integer "project_id"
+    t.integer "user_id"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
