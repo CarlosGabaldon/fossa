cbb08e56750176142d6005659c37db1c11425825
Carlos Gabaldon <cgabaldon@gmail.com>
2012-04-29 10:55:23 -0700

    Issue #1, #2

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,7 @@ class ProjectsController < ApplicationController
   
   def get_projects
     user = User.find(User.first)
+    @current_user = user
     @projects = user.projects
     @high_projects = user.projects.high_priority
     @med_projects = user.projects.med_priority
